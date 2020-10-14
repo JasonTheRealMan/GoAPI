c3abea206793126bc923f210a024b87aa9c0fa31
JasonTheMan <xdropyt@gmail.com>
2020-10-14 23:10:20 +0200

    article func, i feel like this isnt as hard as i thought. But its just the start so im not sure

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 // importing shit
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+	Content     string `json:"Description"`
+}
+
+var Articles []Article
+
+func allarticles(w http.ResponseWriter, e *http.Request) {
+	articles := Articles{
+		Article{Title: "First Article", Description: "blablablablabla", Content: "ehm"},
+	}
+	fmt.Println("allarticle end")
+	json.NewEncoder(w).Encode(articles)
+}
 
 // The connection with the homepage
 func homepage(w http.ResponseWriter, r *http.Request) {
@@ -16,6 +33,8 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 // This is the function that will handle the requests
 func handlerequests() {
 	http.HandleFunc("/", homepage)
+	// Lets handle the articles func
+	http.HandleFunc("/articles", allarticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
